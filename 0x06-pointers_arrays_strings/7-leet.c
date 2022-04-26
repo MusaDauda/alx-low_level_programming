7e07279dc841340080b9d7e09c945157a1be2bbe
Musa Dauda <marshalmusa143@gmail.com>
2022-04-25 17:22:06 -0700

    a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,7 +14,7 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; l[j]; != '\0'; j++)
+		for (j = 0; l[j] != '\0'; j++)
 		{
 			if (s[i] == l[j] || s[i] == (l[j] - 32))
 			{
