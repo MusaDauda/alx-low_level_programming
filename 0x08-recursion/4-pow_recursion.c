3df6ef669b8901a0c0b23e47f7d5af154fc93b31
Musa Dauda <marshalmusa143@gmail.com>
2022-04-28 21:51:35 -0700

    a function that returns the value of x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,9 +4,10 @@
  * _pow_recursion - pow recursion
  * @x: integer params
  * @y: integer paramtr
+ */
 #include "main.h"
 
-/**
+ /*
  * _pow_recursion - pow recursion
  * @x: integer params
  * @y: integer paramtr
