a131f40dd8d7dc7c015a8e211fa062c4496ae7ed
Musa Dauda <marshalmusa143@gmail.com>
2022-05-25 01:47:46 +0100

    print all elements of list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,23 +4,22 @@
 /**
  * print_list - print all the elements of list_t
  * @h: the head of the list
- *
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    if (!h)
-        return (0);
-    while (h)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        nodes += 1;
-        h = h->next;
-    }
-    return (nodes);
+	if (!h)
+		return (0);
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		nodes += 1;
+		h = h->next;
+	}
+	return (nodes);
 }
